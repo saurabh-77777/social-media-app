42e99bc562af7c07b1523648c5b7da60df2155e3
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-02-16 19:53:35 +0530

    Update API URL to use environment variable

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://social-media-app-1-gkia.onrender.com/api/v1/login", data,
+      const res = await axios.post("import.meta.env.VITE_API-URL/api/v1/login", data,
   {
     withCredentials: true, 
   });
