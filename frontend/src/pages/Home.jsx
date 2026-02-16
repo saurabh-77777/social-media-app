a359448a03ae2f7c2773ed50f10f91a425031494
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-02-16 19:55:54 +0530

    Update API URL to use environment variable

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
     const fetchData = async () => {
       try {
         const res = await axios.get(
-          "http://localhost:5000/api/v1/profile",
+          "import.meta.env.VITE_API-URL/api/v1/profile",
           { withCredentials: true }
         );
         setUser(res.data);
