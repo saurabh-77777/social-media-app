67bcbdd622046f18cb485d32c1ab58c6a502746a
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-02-16 19:54:16 +0530

    Update API URL in Register component

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -24,7 +24,7 @@ const Register = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/v1/register",
+        "import.meta.env.VITE_API-URL/api/v1/register",
         data,
       );
 
