ad18e53d27ecf9cee10119acdca621aa9b417588
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-02-17 11:53:04 +0530

    Update API endpoint for fetching user profile

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -19,11 +19,7 @@ const EditProfile = () => {
     const fetchProfile = async () => {
       try {
         const res = await axios.get(
-<<<<<<< HEAD
           `${API}/api/v1/profile`,
-=======
-          "https://social-media-app-2-m298.onrender.com/api/v1/profile",
->>>>>>> d6dce7d0cfe056023ccc73c6117c325b825c7966
           { withCredentials: true }
         );
 
