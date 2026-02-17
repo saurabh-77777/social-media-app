7aeebca776d30a122939a65d477f4b724314979b
saurabh-77777 <153528949+saurabh-77777@users.noreply.github.com>
2026-02-17 12:05:08 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "http://social-media-app-seven-ashy-32.vercel.app", 
     credentials: true,             
   })
 );
